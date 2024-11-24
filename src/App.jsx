77cf72791bdcdd781fe97871f236467715f3fa99
jovanlekic12 <lekicjovan1212@gmail.com>
2024-11-24 18:58:54 +0100

    add reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
 import "./App.css";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
